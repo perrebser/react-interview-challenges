6e895d90dee4a92a12122265f45f8b6cb6ba1b74
perrebser <workperrebser@gmail.com>
2023-11-01 14:43:54 +0100

    use 2 effects for img and fact

--- 01-trainee-challenge/src/App.jsx
+++ 01-trainee-challenge/src/App.jsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
+import  "./index.css";
+
 function App() {
+  const RANDOM_FACTS_END = "https://catfact.ninja/fact";
+  // const RANDOM_IMG_END = `/cat/says/${world}?fontSize=400&fontColor=blue`;
   
+  const [fact, setFact] = useState("");
+  const [imageUrl,setImageUrl]=useState();
+  
+  useEffect(() => {
+    fetch(RANDOM_FACTS_END)
+      .then((response) => response.json())
+      .then((result) => setFact(result.fact));
+     
+  }, []);
+
+  useEffect(()=>{
+    const world=fact.split(' ')[0]
+      
+    setImageUrl(`https://cataas.com/cat/says/${world}?fontSize=400&fontColor=blue`)
+
+
+  },[fact])
+  
+  const fetchFact = () => {
+    fetch(RANDOM_FACTS_END)
+      .then((response) => response.json())
+      .then((result) => setFact(result.fact));
+  };
+
+
 
   return (
-    <div>
-      <p>hola mundo</p>
+    <div className="wrap">
+      <button
+        onClick={() => {
+          fetchFact();
+        }}
+      >
+        Get random fact
+      </button>
+      <p>
+        <strong>Random Fact: </strong>
+        {fact && <p>{fact}</p> }
+      </p>
+      
+        {imageUrl && <img src={imageUrl} alt={`Image of cat saying first world from ${fact}`}></img> }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
