1b2d7ccd3b9875593e071067317c6827db54519a
perrebser <workperrebser@gmail.com>
2023-11-07 19:37:48 +0100

    refactor add form event

--- 02-add-list-junior-challenge/src/App.tsx
+++ 02-add-list-junior-challenge/src/App.tsx
@@ -3,54 +3,58 @@ import "./App.css";
 import { ListItem } from "./types";
 
 function App() {
-  const [value, setValue] = useState<string>('');
-
   const [list, setList] = useState<ListItem>([]);
 
-  const handleAddToList = (title: string):void => {
-    if (title === undefined || "") return;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault(); //in order to prevent the page from reloading on the form onSubmit event
+    const { elements } = event.currentTarget;
+    const input = elements.namedItem("inputElement");
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || input == null) return;
     const newItem = {
       id: crypto.randomUUID(),
-      title: title,
+      title: input.value,
     };
-    const newList = list.concat(newItem);
-    setList(newList);
-    setValue("");
-  };
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
-    setValue(e.target.value);
+    setList(() => {
+      return [...list, newItem];
+    });
+    input.value = " ";
   };
 
-  const handleRemove = (id:string):void=>{
-    const newList=list.filter((item) =>
-      item.id!==id
-    )
-    setList(newList)
-  }
+  const handleRemove = (id: string): void => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
   return (
     <main>
       <header>
-        <input
-          placeholder="Type something..."
-          value={value}
-          onChange={(e) => {
-            handleOnChange(e);
-          }}
-        ></input>
-        <button
-          onClick={() => {
-            handleAddToList(value);
-          }}
-        >
-          Add to list
-        </button>
+        <h2>Todo list react interview</h2>
       </header>
-      <h2>Add to list</h2>
-      <ul>
-        {list.map((item) => (
-          <li onClick={()=>{handleRemove(item.id)}} key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <input name="inputElement" placeholder="Type something..."></input>
+        <button type="submit">Add to list</button>
+      </form>
+      <section>
+        <h2>Element List</h2>
+        <ul>
+          {list.length === 0 ? (
+            <p>
+              <strong>Empty list!</strong>
+            </p>
+          ) : (
+            list.map((item) => (
+              <li
+                onClick={() => {
+                  handleRemove(item.id);
+                }}
+                key={item.id}
+              >
+                {item.title}
+              </li>
+            ))
+          )}
+        </ul>
+      </section>
     </main>
   );
 }
