d9b92c2af821ba55c0b2274a6acbe5ef7ed0d36e
perrebser <workperrebser@gmail.com>
2023-11-24 19:49:02 +0100

    remove from reading list

--- 03-reading-list/src/App.tsx
+++ 03-reading-list/src/App.tsx
@@ -5,62 +5,80 @@ import data from "./const/books.json";
 import SideBar from "./components/SideBar";
 
 function App() {
-
-  const [books,setBooks] = useState<Book[]>(
-    data.library.map((book) => book.book)
+  const [books, setBooks] = useState<Book[]>(
+    data.library.map((book)=>book.book)
   );
 
   const [readingListCount, setReadingListCount] = useState<number>(0);
 
+  const [toggleRender, setToggleRender] = useState<number>(1);
+
   const [readingList, setReadingList] = useState<Book[]>([]);
 
-  const[avaliableBooks,setAvaliableBooks]= useState<number>(books.length)
+  const [avaliableBooks, setAvaliableBooks] = useState<number>(books.length);
 
-  const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
+  const [renderedBooks, setRenderedBooks] = useState<Book[]>([]);
 
   const [filter, setFilter] = useState<string>("");
 
-  const[ numPagesFilter,setNumPagesFilter]=useState<number>(200)
+  const [numPagesFilter, setNumPagesFilter] = useState<number>(200);
 
-  const handleChangeFilter = (event:React.ChangeEvent<HTMLSelectElement>): void => {
+
+
+  const handleChangeFilter = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
     setFilter(event.target.value);
   };
 
-  const handleChangeFilterPages=(event:React.ChangeEvent<HTMLInputElement>):void=>{
-    const maxPages:number=event.target.valueAsNumber
-    setNumPagesFilter(maxPages)
-    const filteredBooks=books.filter((element)=>element.pages<=maxPages);
-    setFilteredBooks(filteredBooks);
-  }
-
-  const handleAddToReadingList = (ISBN: string): void => {
-    //TODO need to check if books is in reading list -> delete from reading, if not -> add to reading list //rerender avaliable & readingList
-    const book = books.find((element) => element.ISBN === ISBN);
-    if(book){
-      setReadingList([...readingList, book!]);
-      const avaliableBooks=books.filter((element)=>element.ISBN!==ISBN)
-      setBooks(avaliableBooks)
-    }else{
-      const ereadingList=readingList.filter((element)=>element.ISBN!==ISBN)
-      setReadingList(ereadingList)
-      console.log(readingList)
-      console.log(books)
-    }
+  const handleChangeFilterPages = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const maxPages: number = event.target.valueAsNumber;
+    setNumPagesFilter(maxPages);
+    const filteredBooks = books.filter((element) => element.pages <= maxPages);
+    setRenderedBooks(filteredBooks);
   };
 
+  const handleBookClick = (ISBN: string): void => {
+    const book = books.find((element) => element.ISBN === ISBN);
+    if (book) {
+      setReadingList([...readingList, book!]);
+      const avaliableBooks = books.filter((element) => element.ISBN !== ISBN);
+      setBooks(avaliableBooks);
+    } else {
+      handleDeleteFromReadingList(ISBN);
+    }
+  };
+  const handleDeleteFromReadingList = (ISBN: string): void => {
+    const updatedReadingList = readingList.filter(
+      (element) => element.ISBN !== ISBN
+    );
+    setReadingList(updatedReadingList);
+    const bookDelete = readingList.find((element) => element.ISBN === ISBN);
+    setBooks([...books, bookDelete!]);
+  };
   useEffect(() => {
     setReadingListCount(readingList.length);
-    setAvaliableBooks(books.length)
-  }, [readingList,books]);
+    setAvaliableBooks(books.length);
+  }, [readingList, books]);
 
   useEffect(() => {
     if (filter === "") {
-      setFilteredBooks(books);
+      setRenderedBooks(books);
     } else {
       const filterBooks = books.filter((book) => book.genre === filter);
-      setFilteredBooks(filterBooks);
+      setRenderedBooks(filterBooks);
     }
   }, [filter, books]);
+  useEffect(() => {
+    if (toggleRender === 1) {
+      setRenderedBooks(books);
+    } else if (toggleRender === 0) {
+      setRenderedBooks(readingList);
+    }
+  }, [toggleRender, books, readingList]);
+
   return (
     <>
       <main>
@@ -71,20 +89,31 @@ function App() {
         </nav>
         <section className="content">
           <div className="flex mb-3 justify-items-start gap-4 border-b-2 border-white">
-            <h2 className="text-2xl cursor-pointer hover:text-white" onClick={()=>setFilteredBooks(books)}>
+            <h2
+              className="text-2xl cursor-pointer hover:text-white"
+              onClick={() => {
+                setToggleRender(1);
+              }}
+            >
               Libros disponibles({avaliableBooks})
             </h2>
-            <h2 className="text-2xl cursor-pointer hover:text-white" onClick={()=>setFilteredBooks(readingList)}>
+            <h2
+              className="text-2xl cursor-pointer hover:text-white"
+              onClick={() => {
+                setToggleRender(0);
+              }}
+            >
               Lista de Lectura({readingListCount})
             </h2>
           </div>
-          <Books
-            books={filteredBooks}
-            onAddToReadingList={handleAddToReadingList}
-          />
+          <Books books={renderedBooks} onBookClick={handleBookClick} />
         </section>
         <aside>
-          <SideBar onChangeFilter={handleChangeFilter} onFilterPage={handleChangeFilterPages} numPagesFilter={numPagesFilter} />
+          <SideBar
+            onChangeFilter={handleChangeFilter}
+            onFilterPage={handleChangeFilterPages}
+            numPagesFilter={numPagesFilter}
+          />
         </aside>
       </main>
     </>
--- 03-reading-list/src/components/Books.tsx
+++ 03-reading-list/src/components/Books.tsx
@@ -1,47 +1,57 @@
 import "../App.css";
 
-
-export interface Book{
-  title:string,
-  pages:number,
-  genre:string,
-  cover:string,
-  synopsis:string,
-  year:number,
-  ISBN:string,
-  author:{
-    name:string,
-    otherBooks:string[]
-  }
+export interface Book {
+  title: string;
+  pages: number;
+  genre: string;
+  cover: string;
+  synopsis: string;
+  year: number;
+  ISBN: string;
+  author: {
+    name: string;
+    otherBooks: string[];
+  };
 }
 
-interface Props{
-  books:Book[];
-  onAddToReadingList:(ISBN: string)=>void;
+interface Props {
+  books: Book[];
+  onBookClick: (ISBN: string) => void;
 }
-const Books: React.FC<Props> = ({books,onAddToReadingList}) => {
+const Books: React.FC<Props> = ({ books, onBookClick }) => {
   return (
     <>
-    {
-      books.length==0 ?
-      <span>No hemos encontrado libros</span>
-      :
-      <ul className="grid grid-cols-1 md:grid-cols-6 gap-x-3">
-        {books.map((book) => (
-          <li  key={book.ISBN} className="flex justify-center flex-col w-52 cursor-pointer" onClick={()=>onAddToReadingList(book.ISBN)}>
-            <p className="uppercase line-clamp-1 font-bold">{book.title}</p>
-            <div className="img-content">
-            <img src={book.cover} className="aspect-9/16 object-cover" width={200}></img>
-            <div className="image-overlay"></div>
-            </div>
-            <span>
-              <p className="text-sm font-semibold">{book.year}</p>
-              <p className="text-sm font-semibold capitalize">{book.pages} pág.</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-    }
+      {books.length == 0 ? (
+        <div className="flex items-center justify-center mt-4">
+          <span className="font-normal text-xl">Sin libros en la lista de lectura</span>
+        </div>
+      ) : (
+        <ul className="grid grid-cols-1 md:grid-cols-6 gap-x-3">
+          {books.map((book) => (
+            <li
+              key={book.ISBN}
+              className="flex justify-center flex-col w-52 cursor-pointer"
+              onClick={() => onBookClick(book.ISBN)}
+            >
+              <p className="uppercase line-clamp-1 font-bold">{book.title}</p>
+              <div className="img-content">
+                <img
+                  src={book.cover}
+                  className="aspect-9/16 object-cover"
+                  width={200}
+                ></img>
+                <div className="image-overlay"></div>
+              </div>
+              <span>
+                <p className="text-sm font-semibold">{book.year}</p>
+                <p className="text-sm font-semibold capitalize">
+                  {book.pages} pág.
+                </p>
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
