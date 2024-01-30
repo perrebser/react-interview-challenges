2042c1302632a783d15be1bff18ac47f842fad7c
perrebser <workperrebser@gmail.com>
2024-01-30 20:12:30 +0100

    search character

--- 05-starwars-app/hooks/usePeople.tsx
+++ 05-starwars-app/hooks/usePeople.tsx
@@ -1,35 +1,45 @@
 export function usePeople() {
   const API_URL = "https://swapi.dev/api/people/";
+  const API_URL_Query = "https://swapi.dev/api/people/?search=";
 
   const getCharacters = async () => {
-    try{
-        const response = await fetch(API_URL)
-        const data=await response.json();
-        return data
-    }catch(error){
-        return []
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return [];
     }
   };
 
-  const getCharacterDetails= async (id:number)=>{
-    try{
-        const response=await fetch(API_URL+id)
-        const data=await response.json()
-        return data
-    }catch(error){
-            return []
+  const getCharacterDetails = async (id: number) => {
+    try {
+      const response = await fetch(API_URL + id);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return [];
     }
-  }
+  };
+  const getCharacterByName = async (query: string) => {
+    try {
+      const response = await fetch(API_URL_Query + query);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return [];
+    }
+  };
 
-  const getNextPage=async(pageUrl:string|undefined)=>{
-    if (pageUrl==undefined) return;
-    try{
-      const response=await fetch(pageUrl)
-      const data = await response.json()
-      return data
-    }catch(error){
-      return []
+  const getNextPage = async (pageUrl: string | undefined) => {
+    if (pageUrl == undefined) return;
+    try {
+      const response = await fetch(pageUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return [];
     }
-  }
-  return {getCharacters,getCharacterDetails,getNextPage}
+  };
+  return { getCharacters, getCharacterDetails, getNextPage,getCharacterByName };
 }
--- 05-starwars-app/src/App.tsx
+++ 05-starwars-app/src/App.tsx
@@ -6,11 +6,12 @@ function App() {
   const [characterResponse, setCharacterResponse] = useState<apiResponse>();
   const [currentCharacterId, setCurrentCharacterId] = useState<number>(1);
   const [characterDetails, setcharacterDetails] = useState<characterInfo>();
-  const { getCharacters, getCharacterDetails, getNextPage } = usePeople();
+  const { getCharacters, getCharacterDetails, getNextPage,getCharacterByName } = usePeople();
   const [previousPageUrl, setPreviousPageUrl] = useState<string>();
   const [nextPageUrl, setNextPageUrl] = useState<string>();
   const [nextPageId, setNextPageId] = useState<string>();
   const [previousPageId, setPreviousPageId] = useState<string>();
+  const [userSearch, setUserSearch]=useState<string>('');
 
   useEffect(() => {
     getCharacters().then((data) => setCharacterResponse(data));
@@ -40,6 +41,17 @@ function App() {
     }
   };
 
+const handleInputChange=(e:React.ChangeEvent<HTMLInputElement>):void=>{
+  setUserSearch(e.currentTarget.value)
+}
+
+useEffect(()=>{
+const timeout=setTimeout(()=>{
+  getCharacterByName(userSearch).then((data)=>setCharacterResponse(data))
+},400)
+return ()=>clearTimeout(timeout)
+},[userSearch])
+
   useEffect(() => {
     getCharacterDetails(currentCharacterId).then((data) =>
       setcharacterDetails(data)
@@ -47,55 +59,63 @@ function App() {
   }, [currentCharacterId]);
 
   return (
-    <div className="flex justify-between ">
-      {characterResponse && parseInt(characterResponse.count) > 0 ? (
-        <div className="flex flex-col gap-1 items-center">
-          <ul className="flex flex-col gap-3">
-            {characterResponse.results.map((p) => (
-              <li
-                className="hover:text-red-300 cursor-pointer"
-                onClick={() => handleOnClick(p)}
-                key={p.name}
-              >
-                {p.name}
-              </li>
-            ))}
-          </ul>
-          <div className="mt-4 flex gap-2">
-            <div className="flex flex-col items-center">
-              <button id="previous-btn" onClick={(e) => handleChangePage(e)}>
-                Previous
-              </button>
-              {previousPageId && <span>{previousPageId}</span>}
+    <div>
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center justify-center gap-3">
+        <label>Search character</label>
+        <input className="mb-8 p-1 rounded-xl" placeholder="Luke Skywalker, R2D2..." value={userSearch} onChange={(e)=>handleInputChange(e)}></input>
+        </div>
+      </div>
+      <div className="flex justify-between ">
+        {characterResponse && parseInt(characterResponse.count) > 0 ? (
+          <div className="flex flex-col gap-1 items-center">
+            <ul className="flex flex-col gap-3">
+              {characterResponse.results.map((p) => (
+                <li
+                  className="hover:text-red-300 cursor-pointer"
+                  onClick={() => handleOnClick(p)}
+                  key={p.name}
+                >
+                  {p.name}
+                </li>
+              ))}
+            </ul>
+            <div className="mt-4 flex gap-2">
+              <div className="flex flex-col items-center">
+                <button id="previous-btn" onClick={(e) => handleChangePage(e)}>
+                  Previous
+                </button>
+                {previousPageId && <span>{previousPageId}</span>}
+              </div>
+              <div className="flex flex-col items-center">
+                <button id="next-btn" onClick={(e) => handleChangePage(e)}>
+                  Next
+                </button>
+                {nextPageId && <span>{nextPageId}</span>}
+              </div>
             </div>
-            <div className="flex flex-col items-center">
-              <button id="next-btn" onClick={(e) => handleChangePage(e)}>
-                Next
-              </button>
-              {nextPageId && <span>{nextPageId}</span>}
+            <span className="font-bold">
+              Total results: {characterResponse.count}
+            </span>
+          </div>
+        ) : (
+          <>
+            <p>No results found!</p>
+          </>
+        )}
+        {characterDetails && (
+          <div>
+            <h2 className="text-xl">Character Details</h2>
+            <div className="mt-2 flex flex-col gap-1">
+              <h3>Name: {characterDetails.name}</h3>
+              <p>Eye color: {characterDetails.eye_color}</p>
+              <p>Gender: {characterDetails.gender}</p>
+              <p>Mass: {characterDetails.mass}kg</p>
+              <p>Height: {characterDetails.height}cm</p>
             </div>
           </div>
-          <span className="font-bold">
-            Total results: {characterResponse.count}
-          </span>
-        </div>
-      ) : (
-        <>
-          <p>No results found!</p>
-        </>
-      )}
-      {characterDetails && (
-        <div>
-          <h2 className="text-xl">Character Details</h2>
-          <div className="mt-2 flex flex-col gap-1">
-            <h3>Name: {characterDetails.name}</h3>
-            <p>Eye color: {characterDetails.eye_color}</p>
-            <p>Gender: {characterDetails.gender}</p>
-            <p>Mass: {characterDetails.mass}kg</p>
-            <p>Height: {characterDetails.height}cm</p>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
